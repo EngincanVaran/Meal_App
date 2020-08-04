aa93862cfb2341b1b250919c67a74d0a95177c0f
Engincan Varan <engincanvaran@Engincans-MacBook-Pro.local>
2020-08-04 14:21:30 +0300

    Bottom Navigation Bar

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -1,7 +1,4 @@
-import 'package:Meal_App/screens/favorites_screen.dart';
 import 'package:flutter/material.dart';
-
-import 'categories_screen.dart';
 
 class TabsScreen extends StatefulWidget {
   @override
@@ -9,34 +6,28 @@ class TabsScreen extends StatefulWidget {
 }
 
 class _TabsScreenState extends State<TabsScreen> {
+  void _selectPage(int index) {}
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      initialIndex: 0, // default starting tab
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text("Meals"),
-          bottom: TabBar(
-            tabs: <Widget>[
-              Tab(
-                icon: Icon(Icons.category),
-                text: "Categories",
-              ),
-              Tab(
-                icon: Icon(Icons.star),
-                text: "Favorites",
-              ),
-            ],
-          ),
-        ),
-        body: TabBarView(
-          children: <Widget>[
-            CategoriesScreen(),
-            FavoritesScreen(),
-          ],
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Meals"),
       ),
+      body: null,
+      bottomNavigationBar: BottomNavigationBar(
+          onTap: _selectPage,
+          backgroundColor: Theme.of(context).primaryColor,
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.category),
+              title: Text("Categories"),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.star),
+              title: Text("Favorites"),
+            )
+          ]),
     );
   }
 }
