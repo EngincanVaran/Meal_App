7ba365a4171d0c4e7547ece3def641cc2eba39fa
Engincan Varan <engincanvaran@Engincans-MacBook-Pro.local>
2020-08-04 14:29:47 +0300

    Dummy

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,8 @@ import 'screens/categories_screen.dart';
 import 'screens/category_meals_screen.dart';
 import 'screens/meal_detail_screen.dart';
 
+// Dummy Code
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
